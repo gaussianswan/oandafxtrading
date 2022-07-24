b97ee869c3d35e0d0a1c37ec7faf9d3aa8468b10
Stephon Henry-Rerrie <srerrie@gmail.com>
2022-07-24 14:35:12 -0400

    added base class for the strategies

--- strategies.py
+++ strategies.py
@@ -3,21 +3,51 @@ import pandas as pd
 import numpy as np
 from historicaldata import OANDAHistoricalData
 
+class OANDABacktestStrategy: 
+    
+    def __init__(self, instrument: str, start_date: str, end_date: str, granularity: str, long_short = True): 
+        self._instrument = instrument
+        self._start_date = start_date
+        self._end_date = end_date
+        self._granularity = granularity
+        self._long_short = long_short
+        self._historical_data = OANDAHistoricalData(instrument=instrument, start_date=start_date, end_date=end_date, granularity=granularity, price_type='M')
 
-class OANDASMAStrategy: 
+    def get_strategy_statistics(self, results: pd.DataFrame) -> dict:  
+        """Calculates strategy statistics and returns it as a dictionary
 
+        Args:
+            results (pd.DataFrame): Results of a backtest in a dataframe
+
+        Returns:
+            dict: _description_
+        """
+
+        total_return = np.exp(results['strategy_return'].sum()) - 1
+        daily_volatility = results['strategy_return'].std()
+        annualized_volatility = daily_volatility * np.sqrt(252)
+        annualized_return = results['strategy_return'].mean() * 252
+        sharpe = annualized_return / annualized_volatility
+
+        summary = {
+            'total_return': total_return, 
+            'annualized_return': annualized_return, 
+            'annualized_vol': annualized_volatility, 
+            'sharpe': sharpe
+        }
+
+        return summary
+
+
+class OANDASMAStrategy(OANDABacktestStrategy): 
+    
     def __init__(self, instrument: str, short_period: int, long_period: int, start_date: str, end_date: str, granularity: str, long_short = True): 
 
+        super().__init__(instrument = instrument, start_date = start_date, end_date = end_date, granularity = granularity, long_short = long_short)
         assert short_period < long_period, "The short period has to be shorter in value than the long period"; 
 
-        self._instrument = instrument; 
         self._short_period = short_period; 
         self._long_period = long_period; 
-        self._start_date = start_date; 
-        self._end_date = end_date; 
-        self._long_short = long_short; 
-        self._granularity = granularity; 
-        self._historical_data = OANDAHistoricalData(instrument=instrument, start_date=start_date, end_date=end_date, granularity=granularity, price_type='M')
         self._short_period_col_name = "SMA_{}".format(self._short_period)
         self._long_period_col_name = 'SMA_{}'.format(self._long_period)
 
@@ -54,47 +84,14 @@ class OANDASMAStrategy:
 
         return_cols = ['c', 'log_c', 'log_c_returns', self._short_period_col_name, self._long_period_col_name, 'strategy_position', 'strategy_return', 'strategy_cumulative_return']
         return (self.get_strategy_statistics(candles), candles[return_cols]) 
-        
 
-    def get_strategy_statistics(self, results: pd.DataFrame) -> dict:  
-        """Calculates strategy statistics and returns it as a dictionary
+class OANDAEMAStrategy(OANDABacktestStrategy): 
 
-        Args:
-            results (pd.DataFrame): Results of a backtest in a dataframe
+    def __init__(self, instrument: str, short_halflife: float, long_halflife: float, start_date: str, end_date: str, granularity: str, long_short=True):
+        super().__init__(instrument, start_date, end_date, granularity, long_short)
 
-        Returns:
-            dict: _description_
-        """
-
-        total_return = np.exp(results['strategy_return'].sum()) - 1
-        daily_volatility = results['strategy_return'].std()
-        annualized_volatility = daily_volatility * np.sqrt(252)
-        annualized_return = results['strategy_return'].mean() * 252
-        sharpe = annualized_return / annualized_volatility
-
-        summary = {
-            'total_return': total_return, 
-            'annualized_return': annualized_return, 
-            'annualized_vol': annualized_volatility, 
-            'sharpe': sharpe
-        }
-
-        return summary
-
-class OANDAEMAStrategy: 
-
-    def __init__(self, instrument: str, short_halflife: float, long_halflife: float, start_date: str, end_date: str, granularity: str, long_short = False) -> None:
-        self._instrument = instrument
-
-        assert short_halflife < long_halflife, "The short half life has to be shorter than the long halflife"
         self._short_halflife = short_halflife
         self._long_halflife = long_halflife 
-        self._start_date = start_date
-        self._end_date = end_date
-        self._granularity = granularity
-        self._long_short = long_short
-        self._historical_data = OANDAHistoricalData(instrument=instrument, start_date=start_date, end_date=end_date, granularity=granularity, price_type='M')
-
         self._short_period_col_name = f'EMA_{self._short_halflife}'
         self._long_period_col_name = f'EMA_{self._long_halflife}'
 
@@ -129,32 +126,7 @@ class OANDAEMAStrategy:
         candles['strategy_cumulative_return'] = 1 + candles['strategy_return'].cumsum(); 
 
         return_cols = ['c', 'log_c', 'log_c_returns', self._short_period_col_name, self._long_period_col_name, 'strategy_position', 'strategy_return', 'strategy_cumulative_return']
-        return (self.get_strategy_statistics(candles), candles[return_cols]) 
-
-    def get_strategy_statistics(self, results: pd.DataFrame) -> dict:  
-        """Calculates strategy statistics and returns it as a dictionary
-
-        Args:
-            results (pd.DataFrame): Results of a backtest in a dataframe
-
-        Returns:
-            dict: _description_
-        """
-
-        total_return = np.exp(results['strategy_return'].sum()) - 1
-        daily_volatility = results['strategy_return'].std()
-        annualized_volatility = daily_volatility * np.sqrt(252)
-        annualized_return = results['strategy_return'].mean() * 252
-        sharpe = annualized_return / annualized_volatility
-
-        summary = {
-            'total_return': total_return, 
-            'annualized_return': annualized_return, 
-            'annualized_vol': annualized_volatility, 
-            'sharpe': sharpe
-        }
-
-        return summary
+        return (self.get_strategy_statistics(candles), candles[return_cols])
 
     def __repr__(self) -> str:
 
@@ -180,21 +152,16 @@ class OANDAEMAStrategy:
         return representation
 
 
-class OANDASMAEMAStrategy: 
+class OANDASMAEMAStrategy(OANDABacktestStrategy): 
     """Strategy where look the crossover between an EMA line and an SMA line. If the EMA line is above the
     SMA line, then we are going to be long. If the other, we are short. 
     """
 
     def __init__(self, instrument: str, ema_period: int, sma_period: int, start_date: str, end_date: str, granularity = 'D', long_short = True) -> None:
+        super().__init__(instrument, start_date, end_date, granularity, long_short)
         self._instrument = instrument
         self._ema_period = ema_period
         self._sma_period = sma_period
-        self._start_date = start_date
-        self._end_date = end_date
-        self._granularity = granularity
-        self._long_short = long_short
-
-        self._historical_data = OANDAHistoricalData(instrument=instrument, start_date=start_date, end_date=end_date, granularity=granularity, price_type='M')
         self._ema_col_name = f'EMA_{self._ema_period}'
         self._sma_col_name = f'SMA_{self._sma_period}'
 
@@ -230,31 +197,6 @@ class OANDASMAEMAStrategy:
 
         return_cols = ['c', 'log_c', 'log_c_returns', self._ema_col_name, self._sma_col_name, 'strategy_position', 'strategy_return', 'strategy_cumulative_return']
         return (self.get_strategy_statistics(candles), candles[return_cols]) 
-
-    def get_strategy_statistics(self, results: pd.DataFrame) -> dict:  
-        """Calculates strategy statistics and returns it as a dictionary
-
-        Args:
-            results (pd.DataFrame): Results of a backtest in a dataframe
-
-        Returns:
-            dict: _description_
-        """
-
-        total_return = np.exp(results['strategy_return'].sum()) - 1
-        daily_volatility = results['strategy_return'].std()
-        annualized_volatility = daily_volatility * np.sqrt(252)
-        annualized_return = results['strategy_return'].mean() * 252
-        sharpe = annualized_return / annualized_volatility
-
-        summary = {
-            'total_return': total_return, 
-            'annualized_return': annualized_return, 
-            'annualized_vol': annualized_volatility, 
-            'sharpe': sharpe
-        }
-
-        return summary
         
 
 
