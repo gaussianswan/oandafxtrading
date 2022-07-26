3bbd766cd63a073a46acb40ab9fb10f793fca997
gaussianswan <srerrie@gmail.com>
2022-07-26 18:45:26 -0400

    main sma strategys script instructions

--- sma_strategy_run.py
+++ sma_strategy_run.py
@@ -1,10 +1,19 @@
-from livestrategies import OANDASMALiveStrategy
 import pandas as pd
+from livestrategies import OANDASMALiveStrategy
+from iofuncs import pickle_to_file
 
-strategy_params = {
-    'instrument': 'GBP_USD', 
-    'short_period': 20, 
-    'long_period': 60, 
-    'granularity': pd.Timedelta(minutes = 1), 
-    'trade_size': 10000
+params = {
+    'instrument': 'EUR_USD',
+    'short_period': 10,
+    'long_period': 30,
+    'granularity': pd.Timedelta(seconds = 30),
+    'trade_size': 100000,
+    'long_short': True,
+    'timeout': 21600
 }
+
+sma_strategy = OANDASMALiveStrategy(**params)
+sma_strategy.run()
+print("Starting Strategy Now")
+filename = 'GBP_USD_s10_l30_30seconds.pkl'
+pickle_to_file(sma_strategy, filename=filename)
