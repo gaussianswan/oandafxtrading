0eae08f3c31311a6ff06691616ffe541668facc6
Stephonhr <stephonhr@advancedblockchain.com>
2022-07-26 12:02:41 -0400

    updated oanda trade object

--- oandatrade.py
+++ oandatrade.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-class OANDATrade: 
+class OANDATrade:
 
-    def __init__(self, oanda_fill_dict: dict, timezone = 'US/Eastern'): 
+    def __init__(self, oanda_fill_dict: dict, timezone = 'UTC'):
 
-        for key, value in oanda_fill_dict.items(): 
+        for key, value in oanda_fill_dict.items():
             setattr(self, f"{key}", value)
 
         self.time = pd.Timestamp(self.time, tz=timezone)
         self.units = float(self.units)
         self.price = float(self.price)
+        self.pl = float(self.pl)
